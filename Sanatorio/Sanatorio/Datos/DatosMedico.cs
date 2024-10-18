2bb2ada1d00921de4895fde84e2959e6c7aaa019
JulietaMena26 <julimartinamena@gmail.com>
2024-10-18 11:14:05 -0300

    Interfaz-Datosmedico
    Se creo la interzas para medico y la clase datosMedico

---
+++ Sanatorio/Sanatorio/Datos/DatosMedico.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanatorio.Interfaz;
+using Sanatorio.Modelos;
+
+namespace Sanatorio.Datos
+{
+    class DatosMedico : IMedico
+    {
+        public bool actualizarMedico(Medico medico)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool agregarMedico(Medico medico)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medico buscarMedicoDni(string dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medico buscarMedicoId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void eliminarMedico(string dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Medico> listadoMedicoApellido(string apellido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Medico> listarMedico()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Medico> listarMedicoXespecialidad(string especialidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sanatorio/Sanatorio/Interfaz/IMedico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanatorio.Modelos;
+
+namespace Sanatorio.Interfaz
+{
+    internal interface IMedico
+    {
+        bool agregarMedico(Medico medico);
+        bool actualizarMedico(Medico medico);
+        void eliminarMedico(string dni);
+        Medico buscarMedicoDni(string dni);
+        Medico buscarMedicoId(int id);
+
+        List<Medico> listadoMedicoApellido(string apellido);
+        List<Medico> listarMedico();
+        List<Medico> listarMedicoXespecialidad(string especialidad);
+
+    }
+}
